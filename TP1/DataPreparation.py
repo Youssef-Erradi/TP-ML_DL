b04eba1ec6f869f5f4decf9b7e021791835e5814
Youssef Erradi <74113158+Youssef-Erradi@users.noreply.github.com>
2022-03-09 20:16:29 +0100

    tp1 final commit

--- TP1/DataPreparation.py
+++ TP1/DataPreparation.py
@@ -1,7 +1,6 @@
 # encoding:utf-8
 # page 1
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 # page 2
@@ -12,20 +11,23 @@ x = dataset.iloc[:,:-1].values
 y = dataset.iloc[:, 3].values
 
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan, strategy="mean").fit(x[:, 1:3])
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
 # page 4
 from sklearn.preprocessing import LabelEncoder
-labelencoder = LabelEncoder()
-x[:, 0] = labelencoder.fit_transform(x[:, 0]) 
-y = labelencoder.fit_transform(y)
+labelencoder_x = LabelEncoder()
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+labelencoder_y = LabelEncoder() 
+y = labelencoder_y.fit_transform(y)
 
 # page 5
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 transform_column = ColumnTransformer([("Catgory", OneHotEncoder(), [0])], remainder="passthrough")
 x = transform_column.fit_transform(x)
+x = x[:, 1:]
 
 # page 6
 from sklearn.model_selection import train_test_split
@@ -35,4 +37,5 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.25, random_
 from sklearn.preprocessing import StandardScaler
 standard_scaler = StandardScaler()
 x_train = standard_scaler.fit_transform(x_train) 
-x_test = standard_scaler.fit_transform(x_test)
+x_test = standard_scaler.transform(x_test)
+print(x_train)
